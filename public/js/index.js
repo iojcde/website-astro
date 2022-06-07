0aba86feb1b2b680b3cdcda7bae996255e820e55
Jeeho Ahn <io@fosshost.org>
2022-06-07 12:36:12 +0000

    new bg

--- public/js/index.js
+++ public/js/index.js
@@ -78,7 +78,7 @@ gsap.fromTo(
   {
     scrollTrigger: {
       trigger: `main`,
-      start: `bottom 7%`,
+      start: `bottom top`,
       scrub: true,
     },
     className: `burger-dark`,
@@ -112,18 +112,6 @@ tl.from('.span-lines .span-line-inner', {
 });
 
 
-const toggleMenu = () => {
-    const menu = document.querySelector('#menu')
-    const overlay=document.querySelector('#overlay')
-
-    menu.classList.toggle('active')
-    overlay.classList.toggle('active')
-}
-
-const button = document.getElementById("nav-button")
-const overlay = document.getElementById("overlay")
-button.onclick = toggleMenu
-overlay.onclick = toggleMenu
 
 const toggleTheme=()=>{
   const isDarkMode = localStorage.getItem('darkMode');
